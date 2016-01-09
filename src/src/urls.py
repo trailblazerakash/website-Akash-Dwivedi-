69cd96380ae060a8edc6e9ccb2c1c56ed2b721af
akash dwivedi <akash.teehnoge@gmail.com>
2016-01-09 16:16:49 +0530

    template adjusted and ra data inserted in website

--- src/src/urls.py
+++ src/src/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'src.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+     url(r'^$', 'website.views.home', name='home'),
+     url(r'^home/', 'website.views.home', name='home'),
+     url(r'^about/', 'website.views.about', name='about'),
+     url(r'^service/', 'website.views.service', name='service'),
+     url(r'^gallery/', 'website.views.gallery', name='gallery'),
+     url(r'^contact/', 'website.views.contact', name='contact'),
+     url(r'^Page_Not_Found/', 'website.views.Page_Not_Found', name='Page_Not_Found'),
+     url(r'^admin/', include(admin.site.urls)),
 )
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ src/website/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request ,'home.html')
+def about(request):
+    return render(request ,'about.html')
+def service(request):
+    return render(request ,'service.html')
+def gallery(request):
+    return render(request ,'gallery.html')
+def contact(request):
+    return render(request ,'contact.html')
+def Page_Not_Found(request):
+    return render(request ,'404.html')
